77431530239c5544819af535de43b27224810c85
Tahmir <Tahmir.robinson@gmail.com>
2026-05-09 21:01:28 -0400

    Refactor backend routes, data, and scoring logic

---
+++ backend/data/data.ts
@@ -0,0 +1,41 @@
+export const jobs = [
+    {
+     id: 1,
+     title: "Software Engineer",
+     company: "Google",
+     location: "Remote",
+     type: "Part-time"
+    },
+    
+    {
+      id: 2,
+      title: "Software Developer",
+      company: "Microsoft",
+      location: "New Jersey",
+      type: "Full-time"
+    },
+
+    {
+      id: 3,
+      title: "Sr. Software Engineer",
+      company: "Sony",
+      location: "Remote",
+      type: "Full-time"
+    },
+
+    {
+      id: 4,
+      title: "Game Developer",
+      company: "Sony",
+      location:"California",
+      type: "Contract"
+    },
+
+    {
+      id: 5,
+      title: "Junior Front-End Developer",
+      company: "Meta",
+      location: "New Jersey",
+      type: "Hybrid"
+    }
+]; 
