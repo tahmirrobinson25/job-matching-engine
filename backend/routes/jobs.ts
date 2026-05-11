0c792286de0a2b4f7e38f306935428fd4128e7ad
Tahmir <Tahmir.robinson@gmail.com>
2026-05-11 15:00:59 -0400

    Add salary scoring to scoring and ranking

--- backend/routes/jobs.ts
+++ backend/routes/jobs.ts
@@ -1,23 +1,24 @@
 import { Router } from 'express';
-import { scoreTitle, scoreLocation, scoreType } from '../logic/utils.ts';
+import { scoreTitle, scoreLocation, scoreType, scoreSalary } from '../logic/utils.ts';
 import { jobs } from '../data/data.ts';
 import type { Request, Response } from 'express';
 
 export const router = Router();
-
-router.get('/',  (req :Request, res :Response) => {
+ router.get('/',  (req :Request, res :Response) => {
     let filterJobs = jobs;
 
     const location = req.query.location as string;
     const title = req.query.title as string;
     const company = req.query.company as string;
     const type = req.query.type as string;
+    const salary = Number(req.query.salary);
 
     const weights = {
       title: 5,
       location: 3,
-      type: 2
-    }
+      type: 2,
+      salary: 4
+    };
 
     if (company) {
       filterJobs = filterJobs.filter((job) =>
@@ -30,7 +31,8 @@ router.get('/',  (req :Request, res :Response) => {
       const scoredTitle = scoreTitle(job.title, title) * weights.title;
       const scoredLocation = scoreLocation(job.location, location) * weights.location;
       const scoredType = scoreType(job.type, type) * weights.type;
-      const score = scoredTitle + scoredLocation + scoredType;
+      const scoredSalary = scoreSalary(job.salary, salary) * weights.salary;
+      const score = scoredTitle + scoredLocation + scoredType + scoredSalary;
       return {...job, score};
     });
 
